a0ed6c51d09f1c9ccd7c669f6b6fedcfcc3ebf72
pandoragear <andy.dang.it@gmail.com>
2021-03-09 23:38:20 -0600

    Fix For to htmlFor

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -11,7 +11,7 @@ const labelText = 'Enter name:';
 
     return (
     <div> 
-        <label className="label" for="name">{labelText}</label>
+        <label className="label" htmlFor="name">{labelText}</label>
         <input id="name" type="text" /> 
         <button style = {{ backgroundColor: 'gray', color: 'white' }}> {buttonText.text} </button> 
     </div> 
