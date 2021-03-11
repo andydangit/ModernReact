e87bc9f27e5145aaf2d82fa9b50d5ce300e1983d
pandoragear <andy.dang.it@gmail.com>
2021-03-10 22:26:03 -0600

    Create Props and calling to CommentDetail

--- components/src/index.js
+++ components/src/index.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-     <CommentDetail author = "Sam" />
-     <CommentDetail author = "Alex" />
-     <CommentDetail author = "Jane" />
-
-
-
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45 PM"
+        textBlog="Nice post"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 1:30 PM"
+        textBlog="This is Awesome"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 2:20 AM"
+        textBlog="Thumb Up"
+        avatar={faker.image.image()}
+      />
     </div>
   );
 };
