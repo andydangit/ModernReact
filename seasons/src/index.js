b0ef929c379ead3c53809a02c705de26c9e205d7
Andy Dang <andy.dang.it@gmail.com>
2021-03-15 19:40:18 -0500

    Add function App

---
+++ seasons/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return <div> Hi There!</div>
+};
+
+ReactDom.render(
+    <App />, 
+    document.querySelector("#root")
+);
