acd1af0cdcf29133b240095519d6cd90e2bf7b85
pandoragear <andy.dang.it@gmail.com>
2021-03-18 20:51:58 -0500

    Add lifecycle component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -24,6 +24,15 @@ class App extends React.Component {
           );
     }
 
+    // example Lifecycle 
+    componentDidMount() {
+        console.log("My component was rendered to the screen");
+    }
+
+    componentDidUpdate(){
+        console.log("My component was just updated -it rerendered!");
+    }
+
 
     // React says we have to define render!!
   render() {
