f0f47c81e9bdbb0eea514cbfdf94ec7611afb28b
pandoragear <andy.dang.it@gmail.com>
2021-03-20 22:20:56 -0500

    Remove unwanted line

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,16 +1,15 @@
 import React from "react";
 
 const seasonConfig = {
-    summer: {
-        text: "Let's hit the beach!",
-        iconName: 'sun'
-    },
-    winter: {
-        text: "Burr, it's cold in here", 
-        iconName: 'snowflake'
-    }
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr, it's cold in here",
+    iconName: "snowflake",
+  },
 };
-
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -22,13 +21,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
- const{ text, iconName} = seasonConfig[season];
+  const { text, iconName } = seasonConfig[season];
 
   return (
     <div>
-    <i className = {`${iconName} icon`} />
-     <h1> {text} </h1> 
-     <i className = {`${iconName} icon`} />
+      <i className={`${iconName} icon`} />
+      <h1> {text} </h1>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
