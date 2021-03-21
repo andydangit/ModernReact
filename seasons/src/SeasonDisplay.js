e8d831278ddc7d25565ead086bb79cee2a093740
pandoragear <andy.dang.it@gmail.com>
2021-03-20 22:19:52 -0500

    Remove Ternary expression and add config option

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+const seasonConfig = {
+    summer: {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr, it's cold in here", 
+        iconName: 'snowflake'
+    }
+};
+
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -10,14 +22,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === "winter" ? "Burr, it\n's cold in here" : "Lets hit the beach";
-  const icon = season ===  'winter'? 'snowflake': 'sun';
+ const{ text, iconName} = seasonConfig[season];
 
   return (
     <div>
-    <i className = {`${icon} icon`} />
+    <i className = {`${iconName} icon`} />
      <h1> {text} </h1> 
-     <i className = {`${icon} icon`} />
+     <i className = {`${iconName} icon`} />
     </div>
   );
 };
