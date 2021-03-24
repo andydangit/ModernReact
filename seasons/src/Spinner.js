6a9d4a30ee168b53cdb477810b15e24bc6a93255
pandoragear <andy.dang.it@gmail.com>
2021-03-23 22:22:37 -0500

    add loader function

--- seasons/src/Spinner.js
+++ seasons/src/Spinner.js
@@ -3,7 +3,10 @@ import React from "react";
 const Spinner = () => {
   return (
     <div className="ui active dimmer">
-      <div className="ui text loader"> Loading </div>
+      <div className="ui big text loader"> Loading.... </div>
     </div>
   );
 };
+
+
+export default Spinner;
