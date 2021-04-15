bd78aa38346948278077a4c8555370a4380fbdc4
pandoragear <andy.dang.it@gmail.com>
2021-04-14 21:39:35 -0500

    call App into index to render

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,3 +1,8 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
 
+ReactDom.render(
+<App />, 
+document.querySelector("#root")
+);
