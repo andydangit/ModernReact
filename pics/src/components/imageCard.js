5310f6c91b11ac036762a33da450bd3c9260494b
pandoragear <andy.dang.it@gmail.com>
2021-05-12 22:13:11 -0500

    Refactor description / urls

--- pics/src/components/imageCard.js
+++ pics/src/components/imageCard.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 
 class ImageCard extends React.Component {
-    render() {
-        return (
-            <div> 
-                <img 
-                alt={this.props.image.description} 
-                src={this.props.image.urls.regular}
-                /> 
-                </div> 
-        );
-    }
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div>
+        <img alt={description} src={urls.regular} />
+      </div>
+    );
+  }
 }
 
 export default ImageCard;
