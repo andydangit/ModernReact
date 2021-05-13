8e10d33817cac0e84450589f26d3fe205575e838
pandoragear <andy.dang.it@gmail.com>
2021-05-12 21:36:09 -0500

    Add css to imagelist.js

--- pics/src/components/ImagesList.js
+++ pics/src/components/ImagesList.js
@@ -1,3 +1,4 @@
+import './ImagesList.css';
 import React from 'react';
 
 const ImageList = (props) => {
@@ -5,7 +6,7 @@ const ImageList = (props) => {
         return <img alt={description} key={id} src={urls.regular} />; 
     });
 
-    return <div> {images} </div> 
+    return <div clasName = "image-list"> {images} </div> 
 };
 
 export default ImageList;
