008078ec91da99f2906ebf66cf35e6252bac0506
pandoragear <andy.dang.it@gmail.com>
2021-05-13 21:44:02 -0500

    Add 'this' state to spans

--- pics/src/components/imageCard.js
+++ pics/src/components/imageCard.js
@@ -4,6 +4,8 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.stae = { spans: 0 };
+
     this.imageRef = React.createRef();
   }
 
@@ -12,8 +14,13 @@ class ImageCard extends React.Component {
   }
 
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
-  }
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 150);
+
+    this.setState({ spans });
+
+  };
 
   render() {
     const { description, urls } = this.props.image;
