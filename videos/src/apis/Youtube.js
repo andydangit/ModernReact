6e504dc5773784ecbe645948687e3eb5d8f31ea6
pandoragear <andy.dang.it@gmail.com>
2021-05-17 21:12:07 -0500

    Youtube API pass to axios

--- videos/src/apis/Youtube.js
+++ videos/src/apis/Youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = "AIzaSyCwznkAnqltFx6SfnRNhJVurN4uBl15xLU";
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: { 
+        part: 'snippet', 
+        maxResults: 5,
+        key: KEY,
+    }
+});
