fe0471af9c0ac7a55ebb764673aeac56251b3891
pandoragear <andy.dang.it@gmail.com>
2021-05-18 21:12:04 -0500

    Create SearchBar top

---
+++ practice/src/component/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+render() { 
+    return(
+        <div>
+            <label> Hi </label>
+        </div>
+    )
+}
+}
+
+export default SearchBar;
