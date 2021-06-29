22d9d540e1017c43f2687d0f3d71db25636adf16
Andy Dang <andy.dang.it@gmail.com>
2021-06-29 16:05:51 -0500

    test different way of UseEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,8 +3,10 @@ import React, { useState, useEffect } from "react";
 const Search = () => {
   const [term, setTerm] = useState("");
 
+  console.log('I RUN WITH EVERY RENDER');
+
   useEffect(() => {
-    console.log('asdlfky');
+    console.log('I RUN AFTER EVERY RENDER AND AT INITIAL RENDER');
   }, [term]);
 
   return (
