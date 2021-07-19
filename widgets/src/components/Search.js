c541896c9287195ccacffbceba9db1895af4e5d9
Andy Dang <andy.dang.it@gmail.com>
2021-07-19 17:35:51 -0500

    add Wikipedia api

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -10,20 +10,17 @@ const Search = () => {
 
     // recommend way to use async in useEffect 
     const search = async () => { 
-      await axios.get('ast');
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
     };
 
-    // different way of using async in useEffect 2nd way 
-  //  ( async() => { 
-  //     await axios.get('');
-  //   })();
-
-
-  // 3rd way to use Async in Use Effect
-    // axios.get('asdf')
-    // .then ((response ) => {
-    //   console.log(response.data);
-    // });
 
     search();
   }, [term]);
