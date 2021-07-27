80898f3c7e0cf822371c4a1c1557b7575d09e7ee
Andy Dang <andy.dang.it@gmail.com>
2021-07-27 14:57:59 -0500

    remove old code and insert Translate

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -34,21 +36,14 @@ const options = [
 ];
 
 export default () => {
-  const [ selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown ] = useState(true);
+  // const [ selected, setSelected] = useState(options[0]);
+  // const [showDropdown, setShowDropdown ] = useState(true);
 
 
 
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)} >Toggle Dropdown </button> 
-{ showDropdown ?
-      <Dropdown 
-      selected={selected} 
-      onSelectedChange={setSelected}
-      options={options} 
-      /> : null
-    }
+      <Translate />
     </div>
   );
 };
