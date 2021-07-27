9d03a2155aea44e815ffc30a9b55fcb5aac51172
Andy Dang <andy.dang.it@gmail.com>
2021-07-26 19:17:15 -0500

    Add useRef

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+
+  const ref = useRef();
 
 
   // this use the click event to lose the drop down 
 useEffect (() => {
   document.body.addEventListener('click', (event) => {
-    console.log(event.target);
+    if (ref.current.contains(event.target)) {
+      return;
+    }
+    
     setOpen(false);
   },
   { capture: true }
@@ -30,8 +35,10 @@ useEffect (() => {
     );
   });
 
+  console.log(ref.current);
+
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
         <div
